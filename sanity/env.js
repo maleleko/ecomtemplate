6730f21ca18b75bcdc669fcebd483c8d9ea485f3
Maleko Osby <whoskoh123@gmail.com>
2023-12-30 15:31:22 -0800

    init

---
+++ sanity/env.js
@@ -0,0 +1,6 @@
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-12-30'
+
+export const dataset = 'production';
+export const projectId = "nwgjyhzx"
+export const useCdn = false
