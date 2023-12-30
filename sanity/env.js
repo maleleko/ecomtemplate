b61b64872b7b8fe692c3ee0d3ac191c4ce5dbed0
maleko osby <106787649+maleleko@users.noreply.github.com>
2023-12-30 15:44:46 -0800

    remove env.js

--- sanity/env.js
+++
@@ -1,6 +0,0 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-12-30'
-
-export const dataset = 'production';
-export const projectId = "nwgjyhzx"
-export const useCdn = false
